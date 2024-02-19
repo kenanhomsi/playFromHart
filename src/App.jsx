2dc5607404ea9942a10618cc1bc51efadf0c2d86
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-19 14:55:51 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,27 @@ import Footer from './/components//Footer'
 import axios from 'axios'
 import { useState } from 'react'
 
-const PlayerClass= await axios.get("https://game-api-hex6.onrender.com/api/v1/playerclass/").then((response)=>{
-    return response;
-}).catch((err)=>console.log(err));
-let PlayerClasslist =PlayerClass.data;
+// const PlayerClass= await axios.get("https://game-api-hex6.onrender.com/api/v1/playerclass/").then((response)=>{
+//     return response;
+// }).catch((err)=>console.log(err));
+// let PlayerClasslist =PlayerClass.data;
 
 function App() {
-  const [playerClassSelected,setPlayerClassSelected]=useState(PlayerClasslist[0].playerclass)
-  const [OrderBtnState,setOrderBtnState]=useState('on');
-  axios.get('https://game-api-hex6.onrender.com/api/v1/MainPage/?banner="main"')
-  .then((res)=>{
-    setOrderBtnState(res.data[0].BayBtn)})
-  .catch((err)=>console.log(err));
+  // const [playerClassSelected,setPlayerClassSelected]=useState(PlayerClasslist[0].playerclass)
+  // const [OrderBtnState,setOrderBtnState]=useState('on');
+  // axios.get('https://game-api-hex6.onrender.com/api/v1/MainPage/?banner="main"')
+  // .then((res)=>{
+  //   setOrderBtnState(res.data[0].BayBtn)})
+  // .catch((err)=>console.log(err));
   return (
     <>
     <div className='App'>
-    <Navbar OrderBtnState={OrderBtnState} />
+    {/* <Navbar OrderBtnState={OrderBtnState} />
     <ListGames setPlayerClassSelecte={(value)=> setPlayerClassSelected(value)}/>
-    <Body  playerClassSelected={playerClassSelected}/>
+    <Body  playerClassSelected={playerClassSelected}/> */}
     <WhoAreWe />
     </div>
-    <Footer OrderBtnState={OrderBtnState} />
+    {/* <Footer OrderBtnState={OrderBtnState} /> */}
     </>
   )
 }
